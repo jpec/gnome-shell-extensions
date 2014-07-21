c7d963d94e63727393abb25ec906ae80acd90255
Julien Pecqueur <jpec@julienpecqueur.net>
2014-07-21 13:33:07 +0200

    transparenttopbar : enable rounded corner

--- transparenttopbar/extension.js
+++ transparenttopbar/extension.js
@@ -8,9 +8,13 @@ function init() {
 function enable() {
     // Add transparency
     Main.panel.actor.add_style_class_name('panel-transparency');
+    Main.panel._leftCorner.actor.add_style_class_name('panel-transparency');
+    Main.panel._rightCorner.actor.add_style_class_name('panel-transparency');
 }
 
 function disable() {
     // Restore opacity
     Main.panel.actor.remove_style_class_name('panel-transparency');
+    Main.panel._leftCorner.actor.remove_style_class_name('panel-transparency');
+    Main.panel._rightCorner.actor.remove_style_class_name('panel-transparency');
 }
