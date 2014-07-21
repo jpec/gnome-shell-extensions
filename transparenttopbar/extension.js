df49c9bd131fa70debea7295cafb25a20837d975
Julien Pecqueur <jpec@julienpecqueur.net>
2014-07-21 13:21:41 +0200

    transparenttopbar : remove duplicate

--- transparenttopbar/extension.js
+++ transparenttopbar/extension.js
@@ -8,11 +8,9 @@ function init() {
 function enable() {
     // Add transparency
     Main.panel.actor.add_style_class_name('panel-transparency');
-    Main.panel.actor.add_style_class_name('panel-transparency');
 }
 
 function disable() {
     // Restore opacity
-    Main.panel.actor.remove_style_class_name('panel-transparency');
     Main.panel.actor.remove_style_class_name('panel-transparency');
 }
